97a40519b50e0f0c3895fadb5461df4d84a2367f
wilkersoh <plgu@hotmail.com>
2021-10-23 09:49:16 +0800

    update: broken pure js

---
+++ main.js
@@ -0,0 +1,69 @@
+var SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, transaction, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) + this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash)
+  }
+}
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "18/10/2021", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty)
+    this.chain.push(newBlock);
+  }
+x
+  isChainValid() {
+    for(let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()) return false;
+
+      if(currentBlock.previousHash !== previousBlock.hash) return false;
+    }
+
+    return true;
+  }
+}
+
+let yzCoin = new BlockChain();
+
+
+
